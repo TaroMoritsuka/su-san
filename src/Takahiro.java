2a1a2eadaae7659d119064f156f7fe864a6d1021
TaroMoritsuka <pepperoni.4.eg@gmail.com>
2019-10-24 15:13:20 +0900

    first commit たかひろクラス

---
+++ src/Takahiro.java
@@ -0,0 +1,19 @@
+
+public class Takahiro {
+	int weight = 98;
+	int height = 165;
+	
+	
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+}
